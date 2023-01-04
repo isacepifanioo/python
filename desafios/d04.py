5d204f867b6498b94a94b8a7e3c103fdb1de820b
isacepifanioo <isacepifanio4@gmail.com>
2023-01-04 14:26:59 -0300

    python

--- desafios/d04.py
+++ desafios/d04.py
@@ -1,7 +1,11 @@
 tex = input('escreva algo: ')
-print('Qual e a clase do ({})'.format(tex), type(tex))
+print('Qual e a clase do {}'.format(tex), type(tex))
+print('A palavra so tem espaço? ', tex.isspace())
 print('A palavra que você digitou so tem numeros? ', tex.isnumeric())
 print('A palavra que você digitou so tem letras? ', tex.isalpha())
 print('A palavra que você digitou tem numeros e letras? ', tex.isalnum())
+print('A palavra esta em maiusculas? ', tex.isupper())
+print('A palavra esta em minusculas? ', tex.islower())
+print('A palavra esta capitalizada? ', tex.istitle())
 
 
