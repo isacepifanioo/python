b888dfcc3028d07cd4998e20df2a0d374ceb1f92
isacepifanioo <isacepifanio4@gmail.com>
2023-01-04 15:50:44 -0300

    python

---
+++ script/ex03.py
@@ -0,0 +1,11 @@
+n1 = int(input('Escreva um valor: '))
+n2 = int(input('segundo valor: '))
+s = n1 + n2
+d = n1 / n2
+p = n1 ** n2
+m = n1 * n2
+r = n1 % n2
+di = n1 // n2
+print('A somas entre o valores e {} a divisão e {:.2f} e a multiplicação {}' .format(s, d, m))
+print('A potencia e {} a divisão inteira e {} O resto da divisão {}' .format(p, di, r))
+
