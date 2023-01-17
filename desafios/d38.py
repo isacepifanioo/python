17120de20b63613c6e769a54f58d8d7c1401fd63
isacepifanioo <isacepifanio4@gmail.com>
2023-01-16 23:07:14 -0300

    python

--- desafios/d38.py
+++ desafios/d38.py
@@ -1,1 +1,14 @@
-#numeros pc
+num = int(input('Digite um numero: '))
+print('''escolha uma opção
+[1] binario
+[2] octal
+[3] hexadecimal''')
+opc = int(input('Sua opção: '))
+if opc == 1:
+    print('{} convertido para binario e igual {}'.format(num, bin(num)[2:]))
+elif opc == 2:
+    print('{} convertido para binario e igual {}'.format(num, oct(num)[2:]))
+elif opc == 3:
+    print('{} convertido para binario e igual {}'.format(num, hex(num)[2:]))
+else:
+    print('Numero invalido')
